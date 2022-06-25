99eb08105bf8d1908ac699e2db218f7367ac4e27
sekulovicjovan <sekulovicjovan12@gmail.com>
2022-06-25 17:53:16 +0200

    IT255-DZ07(Validacija)

--- src/app/room-form/room-form.component.ts
+++ src/app/room-form/room-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Room } from '../models/rooms';
 import { RoomService } from '../services/room.service';
 
@@ -23,10 +23,10 @@ export class RoomFormComponent implements OnInit {
 
     this.myFormGroup = this.myFormBuilder.group({
 
-      id: ['', []],
-      title: ['', []],
+      id: ['', [Validators.required, Validators.minLength(2)]],
+      title: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
       imageUrl: ['', []],
-      price: ['', []]
+      price: ['', [Validators.pattern(/^[1-9][0-9]?$|^100$/), Validators.required]]
     });
   }
 
@@ -46,5 +46,50 @@ export class RoomFormComponent implements OnInit {
 
     this.roomService.addRoom(room);
   }
+  myIdValidation(){
+// pristup inputu
+    const input  = this.myFormGroup.get('id');
+
+return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '' )
+||
+((input.touched || input.dirty) && input.hasError('minlength') ? 'is-invalid' : '' )
+||
+((input.touched || input.dirty) && input.valid ? 'is-valid' : '' );
+
+    }
+
+    myTitleValidation(){
+      // pristup inputu
+          const input  = this.myFormGroup.get('title');
+
+      return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '' )
+      ||
+      ((input.touched || input.dirty) && input.hasError('minlength') ? 'is-invalid' : '' )
+      ||
+      ((input.touched || input.dirty) && input.hasError('maxlength') ? 'is-invalid' : '' )
+      ||
+      ((input.touched || input.dirty) && input.valid ? 'is-valid' : '' );
+
+          }
+
+notValidTitle(){
+  if (this.myFormGroup.get('title').hasError('minlength')) {
+console.log('Mora imati vise od 6 karaktera');
+return 'Minimum length is 6';
+  }else{
+    return '';
+  }
+
+}
+
+myPriceValidation(){
+  const input  = this.myFormGroup.get('price');
+
+  return ((input.touched || input.dirty) && input.hasError('required') ? 'is-invalid' : '' )
+  ||
+  ((input.touched || input.dirty) && input.hasError('pattern') ? 'is-invalid' : '' )
+  ||
+  ((input.touched || input.dirty) && input.valid ? 'is-valid' : '' );
+}
 
 }
